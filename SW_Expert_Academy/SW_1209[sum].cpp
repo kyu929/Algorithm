2366e4d828dd1af5c6de732c310cc3e9f24f6688
yonguk <j2mity@gmail.com>
2021-01-20 18:10:07 +0900

    Update SW_1209[sum].cpp

--- SW_Expert_Academy/SW_1209[sum].cpp
+++ SW_Expert_Academy/SW_1209[sum].cpp
@@ -7,34 +7,59 @@ https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV1
 
 using namespace std;
 
-int arr[101][101];
+int arr[100][100];
 
 int main() {
-	int TestCase = 0;
+	int TestNum = 0;
 	int sum_r = 0;
 	int sum_c = 0;
 	int sum_left = 0;
 	int sum_right = 0;
 	int res = 0;
+	for (int t = 1; t <= 10; t++) {
+		cin >> TestNum;
 
-	for (int col = 0; col < 100; col++) {
-		for (int row = 0; row < 100; row++) {
-			cin >> arr[col][row];
+		sum_left = 0;
+		sum_right = 0;
+		res = 0;
+		for (int col = 0; col < 100; col++) {
+			for (int row = 0; row < 100; row++) {
+				cin >> arr[col][row];
+			}
+		}
+
+		for (int i = 0; i < 100; i++) {
+			sum_r = 0;
+			sum_c = 0;
+			
+			for (int j = 0; j < 100; j++) {
+				sum_r += arr[i][j];
+				sum_c += arr[j][i];
+				if (i == j) {
+					sum_left += arr[i][j];
+					sum_right = arr[99 - i][j];
+				}
+			}
+			if (sum_r > sum_c && sum_r > res) {
+				res = sum_r;
+			}
+			else if (sum_r < sum_c && sum_c > res) {
+				res = sum_c;
+			}
+		}
+		if (sum_left > sum_right && sum_left > res) {
+			res = sum_left;
+			cout << "#" << TestNum << " " << res << endl;
+		}
+		else if (sum_left < sum_right && sum_right < res) {
+			res = sum_right;
+			cout << "#" << TestNum << " " << res << endl;
+		}
+		else {
+			cout << "#" << TestNum << " " << res << endl;
 		}
 	}
 
-	for (int i = 0; i < 100; i++) {
-		sum_r = 0;
-		sum_c = 0;
-		for (int j = 0; j < 100; j++) {
-			sum_r += arr[i][j];
-			sum_c += arr[j][i];
-		}
-		if (sum_r > sum_c && sum_r > res) {
-			res = sum_r;
-		}
-		else if (sum_r < sum_c && sum_c > res) {
-			res = sum_c;
-		}
-	}
+	return 0;
+
 }
