6123e0bf0f03115587ece107da5022bb0d9cbec6
yonguk <j2mity@gmail.com>
2021-02-06 23:49:24 +0900

    Create SW_1244[ing].cpp

---
+++ SW_Expert_Academy/SW_1244[ing].cpp
@@ -0,0 +1,39 @@
+/*
+1244. [S/W 문제해결 응용] 2일차 - 최대 상금
+https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15Khn6AN0CFAYD&categoryId=AV15Khn6AN0CFAYD&categoryType=CODE&problemTitle=2%EC%9D%BC%EC%B0%A8&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void change(string s, int num) {
+
+	int s_size = s.size();
+	int max = 0;
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < s_size; j++) {
+			if (s[j] > max) {
+
+			}
+		}
+	}
+}
+
+int main() {
+
+	int TestCase;
+	string s;
+	int NOE;
+
+	cin >> TestCase;
+
+	for (int i = 1; i < TestCase + 1; i++) {
+		cin >> s >> NOE;
+
+
+	}
+
+}
